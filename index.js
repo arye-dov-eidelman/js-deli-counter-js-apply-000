ba74f316e507118b7d023a75de2a887c6b5cf16d
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-18 19:27:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ function nowServing(deliLine){
 
 }
 function currentLine(deliLine){
-  if (deliLine.length === 0){
-    return 'The line is currently empty.'
-  }
   var result = 'The line is currently: '
-  for (var i = 0; i < deliLine.length; i++) {
+  if (deliLine.length === 0){
+    result += ' empty.'
+  }else{
+    for (var i = 0; i < deliLine.length; i++) {
     result += `${i+1}. ${deliLine[i]}`
-    if (i+1 !== deliLine.length) {
-      result += ', '
+      if (i+1 !== deliLine.length) {
+        result += ', '
+      }
     }
   }
   return result
