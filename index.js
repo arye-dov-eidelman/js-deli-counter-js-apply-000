f32434631c6f05ae97c11da8d2e0f5264aa43e4f
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-18 19:25:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ function takeANumber(deliLine, name){
 }
 
 function nowServing(deliLine){
-  if deliLine.length === 0 return 'There is nobody waiting to be served!'
+  if (deliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  }
   return `Currently serving ${deliLine.shift()}.`;
 
 }
 function currentLine(deliLine){
-  if (deliLine.length === 0) return 'The line is currently empty.'
+  if (deliLine.length === 0){
+    return 'The line is currently empty.'
+  }
   var result = 'The line is currently: ';
   for (var i = 0; i < deliLine.length; i++) {
     result += `${i+1}. ${deliLine[i]}`;
